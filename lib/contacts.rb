3965b72b5dfe2992c213157719679730c4a75275
wei chen <weichen9083@gmail.com>
2019-09-21 00:11:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
    if person == "Freddy Mercury"
      contact_details_hash.each do |attribute, data| 
        binding.pry
-       if attribute == :favorite_ice_cream_flavors data.delete_if 
+       if attribute == :favorite_ice_cream_flavors data.delete_if {|ice_cream| ice_cream == "strawberry"}
        end 
      end 
    end 
